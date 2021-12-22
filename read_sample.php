d87a4af85846fcd4dd3c1e499f085c2bd67316a5
rgenandt <rgenandt@gmail.com>
2021-12-22 17:04:09 +0700

    Update read_sample.php

--- read_sample.php
+++ read_sample.php
@@ -4,5 +4,6 @@ $expr     = "/\[.*\]/";
 $content  = file_get_contents($someFile);
 if($content === FALSE) die('Failed To Read Sample File');
 preg_match_all($expr, $content, $instances);
+// WE COULD MAKE THIS MUCH PRETTIER, FOREACH OVER IT, ETC
 echo 'Text in Brackets: ' . print_r($instances, TRUE) . '<br>';
 ?>
